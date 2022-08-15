4e168c91cf681c7d9b89a0ff2857c57bd16c702d
ecle0005 <ecle0005@student.monash.edu>
2022-08-15 17:33:06 +1000

    create a library with the folder sorting algorithms

---
+++ sorting_algorithms/__init__.py
@@ -0,0 +1,5 @@
+from sorting_algorithms.heap_sort import *
+from sorting_algorithms.merge_sort import *
+from sorting_algorithms.selection_sort import *
+from sorting_algorithms.insertion_sort import *
+from sorting_algorithms.counting_sort import *
